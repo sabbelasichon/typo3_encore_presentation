e6fdacbf48e763f5a122a4192889a31526e9dcf4
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-09-30 21:30:58 +0200

    [TASK] Add basic webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/typo3conf/ext/typo3_webpack_encore_demo/Resources/Public/')
+    .setPublicPath('/typo3conf/ext/typo3_webpack_encore_demo/Resources/Public/')
+    .addEntry('app', './assets/js/Custom.js')
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .autoProvidejQuery();
+
+module.exports = Encore.getWebpackConfig();
